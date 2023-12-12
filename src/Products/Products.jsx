23d5936f6c13eb7d70837db60097c74a40453902
AdamSmith <omercoskun4369@gmail.com>
2023-12-12 17:10:41 +0300

    finito

---
+++ src/Products/Products.jsx
@@ -0,0 +1,8 @@
+// import { Container, Row } from "react-bootstrap";
+import "./Products.css";
+
+const Products = ({ result }) => {
+  return <section className="card-container">{result}</section>;
+};
+
+export default Products;
